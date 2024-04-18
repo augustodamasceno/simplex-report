cae2fb6a94035f7f119e5cc37a34df1866ceb781
augustodamasceno <augustodamasceno@protonmail.com>
2024-04-18 13:16:43 -0300

    refactor: added simplex test structure

---
+++ tests/test_simplex.py
@@ -0,0 +1,19 @@
+import unittest
+import simplex
+
+
+class TestSimplex(unittest.TestCase):
+    @classmethod
+    def setUp(self) -> None:
+        pass
+
+    @classmethod
+    def tearDown(self) -> None:
+        pass
+
+    def test_method0(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
